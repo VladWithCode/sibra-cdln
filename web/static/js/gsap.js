bb327f3ba4ffe724bd198cccd960e59d7b1dd4d6
VladWithCode <vladwithcode@gmail.com>
2024-04-03 17:53:39 -0600

    Add: Js libraries

---
+++ web/static/js/gsap.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6be839c44e0c059635aad30a023fe51d64e593950f4d0565cda5332d8ef44539
+size 71932
---
+++ web/static/js/htmx.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c102c3f09ecd24af50ad1fcc7479ab2af20e5c51b396b414ddf091d4bc5e5b66
+size 47756
