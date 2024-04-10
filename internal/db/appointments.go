406c16722fdba67a4205f95b19aa4f5372d2857a
VladWithCode <vladwithcode@gmail.com>
2024-04-10 13:25:08 -0600

    Add: Appointment DB queries

---
+++ internal/db/appointments.go
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4c3fb9b730e82c91c3decca4a67507ead9dde3fc90a40ba3503039ee33a119a
+size 1867
---
+++ internal/db/db.go
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e86fe462c7f8aab58c2f75e9106f64600900fbb903d6fb667bf4e13e8b15e1b
+size 719
