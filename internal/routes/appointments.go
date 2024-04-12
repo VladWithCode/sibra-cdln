6630176ede49fbc1e69f0043c7b5dd64c7fb2f2f
VladWithCode <vladwithcode@gmail.com>
2024-04-11 19:02:12 -0600

    Fix: Specify Content-Type header for responses
    To aid error handling in client specify the Content-Type header

--- internal/routes/appointments.go
+++ internal/routes/appointments.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb1b389360c7bfe94a18a88c7d395b96f95e3e38ec4d11e34434c6d1be4f5b2d
-size 2789
+oid sha256:33f4c41ffe84ebab34532c75cb14f298f0957be06907e4a01ab81184ad8b1c3d
+size 3087
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eedf26604c5b79ba210ea3ff9927132d57d1558840eeac451f275110f43ff180
-size 1696
+oid sha256:58a2c860e161331426956173fc6b20f3a13e2a82e4c1093bcc2039b820eb8e1b
+size 1834
