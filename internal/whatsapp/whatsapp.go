01c4962e0ea512b43983f313998e38c155f0de6a
VladWithCode <vladwithcode@gmail.com>
2024-04-11 19:03:37 -0600

    Fix: Log only important response data

--- internal/whatsapp/whatsapp.go
+++ internal/whatsapp/whatsapp.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b1a7360e3566180cd891c95639aed5c479da9563714f91d3cf4a1af00750203
-size 3578
+oid sha256:eabf4f1dbb3dacdd7458cdfa519fc25d474983a4654f35cfd131f3b74943ef18
+size 3666
